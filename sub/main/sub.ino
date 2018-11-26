113b510c2924f57d1449d56834b1820e1c43bd70
naoki-cpp <puni87gou@gmail.com>
2018-11-26 14:02:26 +0900

    update

--- sub/main/sub.ino
+++ sub/main/sub.ino
@@ -1,9 +1,23 @@
-void setup()
-{
-	Serial.begin(115200);
+#include <avr/io.h>
+ 
+#define PWMPin 10
+ 
+unsigned int frq = 1000; // 周波数
+float duty = 0.5; // 指定したいデューティ比
+ 
+void setup() {
+  pinMode(PWMPin, OUTPUT);
 }
-
-void loop()
-{
-	analogWrite(5, 126);
+ 
+void loop() {
+ 
+  // モード指定
+  TCCR1A = 0b00100001;
+  TCCR1B = 0b00010010;
+ 
+  // TOP値指定
+  OCR1A = (unsigned int)(1000000 / frq);
+ 
+  // Duty比指定
+  OCR1B = (unsigned int)(1000000 / frq * duty);
 }
